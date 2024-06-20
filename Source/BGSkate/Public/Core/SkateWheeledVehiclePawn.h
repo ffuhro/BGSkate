a5a15269bbd0b593e3b3b84f1af3f198038f505b
Francisco Fuhro <ffuhro@outlook.com>
2024-06-20 04:49:36 -0300

    introduced WheeledVehiclePawn to drive skate movement

---
+++ Source/BGSkate/Public/Core/SkateWheeledVehiclePawn.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "SkateWheeledVehiclePawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BGSKATE_API ASkateWheeledVehiclePawn : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+	
+};
