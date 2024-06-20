f72815f48157e38f549ec651ac6ed53d63300e98
Francisco Fuhro <ffuhro@outlook.com>
2024-06-20 08:12:14 -0300

    First iteration using ChaosVehicle to drive the skate

--- Source/BGSkate/Private/Core/SkateWheeledVehiclePawn.cpp
+++ Source/BGSkate/Private/Core/SkateWheeledVehiclePawn.cpp
@@ -2,4 +2,78 @@
 
 
 #include "Core/SkateWheeledVehiclePawn.h"
+// #include "ChaosVehicleMovementComponent.h"
+#include "ChaosWheeledVehicleMovementComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
+ASkateWheeledVehiclePawn::ASkateWheeledVehiclePawn()
+{
+	GetMesh()->SetSimulatePhysics(true);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->bInheritYaw = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->SocketOffset.Z = 30.f;
+	SpringArm->TargetArmLength = 200.f;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(SpringArm);
+
+	GetMesh()->SetMassOverrideInKg(NAME_None, 3.5f);
+}
+
+void ASkateWheeledVehiclePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// Setup Inputs
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+void ASkateWheeledVehiclePawn::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	// GetVehicleMovementComponent()->AddInputVector(FVector(0.f, 1.f, 0.f));
+}
+
+void ASkateWheeledVehiclePawn::Move(const FInputActionValue& Value)
+{
+	const float CurrentValue = Value.Get<float>();
+	if (Controller != nullptr)
+	{
+		GetVehicleMovementComponent()->SetThrottleInput(CurrentValue);
+	}
+	
+}
+void ASkateWheeledVehiclePawn::Steering(const FInputActionValue& Value)
+{
+	const float CurrentValue = Value.Get<float>();
+	if (Controller != nullptr)
+	{
+		GetVehicleMovementComponent()->SetSteeringInput(CurrentValue);
+	}
+	
+}
+
+void ASkateWheeledVehiclePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("EnhancedInputComponent Success"));
+
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASkateWheeledVehiclePawn::Move);
+		EnhancedInputComponent->BindAction(SteeringAction, ETriggerEvent::Triggered, this, &ASkateWheeledVehiclePawn::Steering);
+	}
+}
--- Source/BGSkate/Public/Core/SkateWheeledVehiclePawn.h
+++ Source/BGSkate/Public/Core/SkateWheeledVehiclePawn.h
@@ -6,6 +6,11 @@
 #include "WheeledVehiclePawn.h"
 #include "SkateWheeledVehiclePawn.generated.h"
 
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+class UCameraComponent;
+class USpringArmComponent;
 /**
  * 
  */
@@ -13,5 +18,39 @@ UCLASS()
 class BGSKATE_API ASkateWheeledVehiclePawn : public AWheeledVehiclePawn
 {
 	GENERATED_BODY()
+
+	ASkateWheeledVehiclePawn();
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+
+// ================================ COMPONENTS =====================================================================
+
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComponent;
+
+// ================================ INPUTS =============================================================================
+protected:
+
+	// Enhanced Input Variables
 	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input)
+	UInputMappingContext* DefaultMappingContext;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input)
+	UInputAction* MoveAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input)
+	UInputAction* SteeringAction;
+
+	// Functions
+
+	void Move(const FInputActionValue& Value);
+	void Steering(const FInputActionValue& Value);
+
+	
+	
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 };
